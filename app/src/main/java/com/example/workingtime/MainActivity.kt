f06a62f9147bbf6894c78c28024563ca9afc9f77
João Pedro Basso Audibert <hotsjoao@gmail.com>
2024-04-09 22:08:30 -0300

    App completed

--- app/src/main/java/com/example/workingtime/MainActivity.kt
+++ app/src/main/java/com/example/workingtime/MainActivity.kt
@@ -1,30 +1,20 @@
 package com.example.workingtime
 
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
-import java.time.LocalDateTime
 
 @RequiresApi(Build.VERSION_CODES.O)
 class MainActivity : AppCompatActivity() {
     private lateinit var timeToCalculate: EditText
-    private lateinit var idealShiftDisplay: TextView
-    private lateinit var maxShiftDisplay: TextView
-    private lateinit var smallExtraDisplay: TextView
-    private lateinit var largeExtraDisplay: TextView
     private lateinit var calculateShiftButton: Button
-    private lateinit var clearButton: Button
-    private val toTimeConverter = ToTimeConverter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,10 +26,6 @@ class MainActivity : AppCompatActivity() {
             calculateShift()
         }
 
-        clearButton.setOnClickListener {
-            clearFields()
-        }
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -49,29 +35,19 @@ class MainActivity : AppCompatActivity() {
 
     private fun startComponents() {
         timeToCalculate = findViewById(R.id.timeToCalculate)
-        idealShiftDisplay = findViewById(R.id.idealShiftDisplay)
-        maxShiftDisplay = findViewById(R.id.maxShiftDisplay)
-        smallExtraDisplay = findViewById(R.id.smallExtraDisplay)
-        largeExtraDisplay = findViewById(R.id.largeExtraDisplay)
         calculateShiftButton = findViewById(R.id.calculateShiftBtn)
-        clearButton = findViewById(R.id.clearBtn)
     }
 
     private fun calculateShift() {
+        val intent = Intent(this, ResultActivity::class.java)
+
         val timeToCalculateShift = timeToCalculate.text.toString()
         val areFieldsValid = validateFields(timeToCalculateShift)
 
         if (areFieldsValid) {
-            val currentTime = toTimeConverter.convertToTime(timeToCalculateShift)
-            val shiftCalculator = ShiftCalculator(currentTime)
-
-            idealShiftDisplay.text = shiftCalculator.calculateShift().toString()
-            maxShiftDisplay.text = shiftCalculator.calculateMaxShift().toString()
-            smallExtraDisplay.text =
-                shiftCalculator.calculateExtraHours(ExtraHourType.SMALL).toString()
-            largeExtraDisplay.text =
-                shiftCalculator.calculateExtraHours(ExtraHourType.LARGE).toString()
+            intent.putExtra("time", timeToCalculateShift)
         }
+        startActivity(intent)
     }
 
     private fun validateFields(timeToValidate: String): Boolean {
@@ -83,13 +59,5 @@ class MainActivity : AppCompatActivity() {
             return false
         }
         return true
-    }
-
-
-    private fun clearFields() {
-        idealShiftDisplay.text = "00:00:00"
-        maxShiftDisplay.text = "00:00:00"
-        smallExtraDisplay.text = "00:00:00"
-        largeExtraDisplay.text = "00:00:00"
     }
 }
--- app/src/main/java/com/example/workingtime/ResultActivity.kt
+++ app/src/main/java/com/example/workingtime/ResultActivity.kt
@@ -1,20 +1,58 @@
 package com.example.workingtime
 
+import android.os.Build
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ResultActivity : AppCompatActivity() {
+    private lateinit var idealShiftDisplay: TextView
+    private lateinit var maxShiftDisplay: TextView
+    private lateinit var smallExtraDisplay: TextView
+    private lateinit var largeExtraDisplay: TextView
+    private lateinit var clearButton : Button
+    private val toTimeConverter = ToTimeConverter()
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_result)
+        startComponents()
+
+        val bundle = intent.extras
+        val timeToCalculateShift = bundle?.getString("time") ?: return
+        val currentTime = toTimeConverter.convertToTime(timeToCalculateShift)
+        val shiftCalculator = ShiftCalculator(currentTime)
+
+        idealShiftDisplay.text = shiftCalculator.calculateShift().toString()
+        maxShiftDisplay.text = shiftCalculator.calculateMaxShift().toString()
+        smallExtraDisplay.text =
+            shiftCalculator.calculateExtraHours(ExtraHourType.SMALL).toString()
+        largeExtraDisplay.text =
+            shiftCalculator.calculateExtraHours(ExtraHourType.LARGE).toString()
+
+        clearButton.setOnClickListener{
+            finish()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun startComponents() {
+        idealShiftDisplay = findViewById(R.id.idealShiftDisplay)
+        maxShiftDisplay = findViewById(R.id.maxShiftDisplay)
+        smallExtraDisplay = findViewById(R.id.smallExtraDisplay)
+        largeExtraDisplay = findViewById(R.id.largeExtraDisplay)
+        clearButton = findViewById(R.id.clearBtn)
     }
 }
